8cf29a2743292e09b79d144fd15d890c9fd71e15
brunoliveirapaes <bruno.paes@buzzbeat.pt>
2021-01-07 18:43:37 +0000

    12th commit

--- js/main.js
+++ js/main.js
@@ -74,8 +74,8 @@ var slider = {
         0:{
             items:1
         },
-        600:{
-            items:2
+        768:{
+            items:1
         },
         1000:{
             items:3
@@ -83,5 +83,6 @@ var slider = {
     }
 });
 
-
-
+function myFunction(x) {
+  x.classList.toggle("change");
+}
