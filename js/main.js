f2d5418f7103d7cd26d47a77874b412fabdd1618
brunoliveirapaes <bruno.paes@buzzbeat.pt>
2021-01-11 12:49:01 +0000

    15th commit

--- js/main.js
+++ js/main.js
@@ -86,3 +86,20 @@ var slider = {
 function myFunction(x) {
   x.classList.toggle("change");
 }
+
+/* accordion */
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
